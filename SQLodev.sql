e890235d711408c7639e51eb3902337fc6e3f7c8
HasanAriff <147430833+HasanAriff@users.noreply.github.com>
2024-03-25 16:47:52 +0300

    Update SQLodev.sql

--- SQLodev.sql
+++ SQLodev.sql
@@ -1,45 +1,68 @@
 create database school_library;
+use school_library;
 create table uyeler(
 uyeNo int not null Primary key identity(1,1),
-uyeAdi nvarchar (50) not null,
-uyeSoyad� nvarchar(30) not null,
-eposta nvarchar(50) not null,
-cinsiyet char(2),
+uyeAdi nvarchar(50) not null,
+uyeSoyadi nvarchar(50) not null,
+eposta nvarchar(100) not null,
+cinsiyet char(2) not null, 
 telefon int
 );
-
-use school_library
+use school_library;
 create table adresler(
-adresNo int not null Primary key identity(1,1),
-sehir nvarchar (50),
-mahalle nvarchar(30),
+adresNo int not null Primary Key identity(1,1),
+sehir nvarchar(50),
+mahalle nvarchar(100),
 binaNo int,
 cadde nvarchar(200),
 ulke nvarchar(50),
 postaKodu int
 );
-
-alter table uyeler add adresNo int constraint fk_uyeler_adresler foreign key (adresNo) references adresler (adresNo);
-
+ALTER TABLE uyeler ADD adresNo int
+constraint FK_uyeler_adres
+FOREIGN KEY (adresNo)
+references ADRESLER (adresNo);
 create table kutuphane(
-kutuphaneNo int not null identity (1,1) primary key,
-kutuphaneIsmi nvarchar(50),
-ac�klama nvarchar(500),
-adresNo int constraint fk_adresler_kutuphane foreign key (adresNo) references adresler (adresNo)
+kutuphaneNo int not null identity(1,1) Primary Key,
+kutuphaneIsmi nvarchar(150),
+aciklama nvarchar(500),
+adresNo int constraint FK_adresler_kutuphane Foreign key(adresNo) references adresler(adresNo)
 );
-
-create table emanet(
-emanetNo int not null identity (1,1) primary key,
+create table Kitaplar(
+ISBN nvarchar(50) not null primary key,
+KITAPADI nvarchar (50),
+SAYFASAYISI int,
+YAYINTARIHI DATETIME
+);
+create table emanetler(
+emanetNo int not null identity(1,1) primary key,
 emanetTarihi datetime,
 teslimTarihi datetime,
-uyeNo int constraint fk_uye_emanet foreign key (uyeNo) references uyeler (uyeNo),
-kutuphaneNo int constraint fk_kutuphane_emanet foreign key (kutuphaneNo) references kutuphane (kutuphaneNo),
-ISBM nvarchar(50) constraint fk_kitaplar_emanet foreign key (ISBM) references kitaplar (ISBM),
+uyeNo int constraint FK_uye_emanet Foreign key(uyeNo) references uyeler(uyeNo),
+kutuphaneNo int constraint FK_kutuphane_emanet Foreign key(kutuphaneNo) references kutuphane(kutuphaneNo),
+ISBN nvarchar(50) CONSTRAINT FK_kitap_emanet FOREIGN KEY(ISBN) REFERENCES Kitaplar(ISBN)
 );
-
-create table kitaplar(
-ISBM nvarchar(50) not null primary key,
-kitapAdi nvarchar(100),
-sayfaSayisi int,
-yayinTarihi datetime,
+create table bulunur(
+miktar int not null identity(1,1) primary key,
+ISBN nvarchar(50) constraint FK_kitap_bulunur foreign key(ISBN) REFERENCES Kitaplar(ISBN),
+kutuphaneNo int constraint FK_kutuphane_bulunur Foreign Key(kutuphaneNo) references kutuphane(kutuphaneNo)
+);
+create table kategoriler(
+kategoriNo int not null identity(1,1) primary key,
+kategoriAdi nvarchar(50),
+);
+create table yazarlar(
+yazarNo int not null identity(1,1) primary key,
+yazarAdi nvarchar(50) not null,
+yazarSoyadi nvarchar(75)not null,
+);
+create table yazar(
+yazar_No int not null identity(1,1) primary key,
+ISBN nvarchar(50) constraint FK_kitap_yazar foreign key(ISBN) REFERENCES Kitaplar(ISBN),
+yazarNo int constraint FK_yazarlar_yazar foreign key(yazarNo) references yazarlar(yazarNo),
+);
+create table BulunurKategori(
+bulunurNo int not null identity(1,1) primary key,
+kategoriNo int constraint FK_kategoriler_BulunurKategori foreign key(kategoriNo) references kategoriler(kategoriNo),
+ISBN nvarchar(50) constraint FK_kitap_BulunurKategori foreign key(ISBN) REFERENCES Kitaplar(ISBN),
 );
